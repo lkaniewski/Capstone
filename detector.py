7156a893cce5a2f5e109f9492668ba4072983f38
Liza Kaniewski <lkaniewski@wustl.edu>
2021-10-31 16:06:40 -0500

    new files, main.py will run and then also print the color detected. Issue is that it prints all circles detected

--- detector.py
+++ detector.py
@@ -1,6 +1,9 @@
 from tensorflow import keras
 from imageai.Detection import ObjectDetection
 import pandas as pd
+
+
+
 detector = ObjectDetection()
 
 model_path = "./models/yolo-tiny.h5"
@@ -18,6 +21,24 @@ for eachItem in detection:
     if eachItem["name"] == "traffic light" and eachItem["percentage_probability"] >= 0.50:
     	test.append(eachItem["name"])
 
-print(test)
+df = pd.DataFrame(detection, columns =['name', 'percentage_probability', 'box_points']) 
+df = df.drop(['box_points'], axis = 1)
+df = df[df.name == 'traffic light']
+num_trafficLight = len(df)
+max_df = df[df.percentage_probability == df.percentage_probability.max()]
+max_score = max_df.iat[0,1]
+print(type(max_score))
+print(num_trafficLight)
 
+def isTrafficLight(num_trafficLight, max_score):
+	if num_trafficLight > 0 and max_score >= 60.00:
+		print('There is a traffic light')
+		return True
+	else: 
+		print("No traffic light")
+		return False
+
+
+
+isTrafficLight(num_trafficLight, max_score)
 
